44c131e8966568a21364d70366069aef4e70e625
Ron Boutilier <ronbout@yahoo.com>
2019-11-11 21:14:54 -0600

    Used SelectField (abstracted version) to redo the TechtagDropdown.js component.  Turned the Skill Search into a react-md Card and worked on some of the styling.  Still a work in progress, but wanted to commit what has been done so far.  Next, working on the Search TextField.

--- src/components/search/SkillSearch/SkillSearch.js
+++ src/components/search/SkillSearch/SkillSearch.js
@@ -4,133 +4,136 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import { convertHtmlToText } from "../../../assets/js/library";
 import "./css/skillSearch.css";
+import { Card, CardTitle, CardText } from "react-md";
 
 const SkillSearch = props => {
-  const handleKeyPress = event => {
-    if (event.key === "Enter") {
-      event && event.preventDefault();
-      props.handleSearch();
-    }
-  };
+	const handleKeyPress = event => {
+		if (event.key === "Enter") {
+			event && event.preventDefault();
+			props.handleSearch();
+		}
+	};
 
-  return (
-    <section className="skill-search p-3">
-      <h1>Skill Search</h1>
+	return (
+		<Card className="skill-search md-block-centered">
+			<CardTitle title="Skill Search" style={{ paddingBottom: "8px" }} />
 
-      {props.state.userMsg && (
-        <div className="skill-search-msg">{props.state.userMsg}</div>
-      )}
-      {props.state.errMsg && (
-        <div className="skill-search-error">{props.state.errMsg}</div>
-      )}
+			<CardText style={{ paddingTop: 0 }}>
+				{props.state.userMsg && (
+					<div className="skill-search-msg">{props.state.userMsg}</div>
+				)}
+				{props.state.errMsg && (
+					<div className="skill-search-error">{props.state.errMsg}</div>
+				)}
 
-      <div className="skill-search-form">
-        {/* techtag <select> for filter */}
+				<div className="skill-search-form">
+					{/* techtag <select> for filter */}
+					<TechtagDropdown
+						tagOptions={props.state.tagOptions}
+						openTagOptions={props.state.openTagOptions}
+						tagSelect={props.state.formFields.tagSelect}
+						handleTagSelect={props.handleTagSelect}
+					/>
 
-        <TechtagDropdown
-          tagOptions={props.state.tagOptions}
-          openTagOptions={props.state.openTagOptions}
-          tagSelect={props.state.formFields.tagSelect}
-          handleTagSelect={props.handleTagSelect}
-        />
-
-        {/* keyword entry */}
-        <div className="form-group">
-          <label htmlFor="keyword">
-            <strong>Search Skills</strong>
-          </label>
-          <div id="skill-search-row" className="row mx-0">
-            <input
-              type="text"
-              className="form-control"
-              name="keyword"
-              id="keyword"
-              value={props.state.formFields.keyword}
-              onChange={props.handleInputChange}
-              onKeyPress={handleKeyPress}
-            />
-            <button
-              type="button"
-              className="btn btn-dark btn-search mx-1"
-              onClick={props.handleSearch}
-            >
-              <FontAwesomeIcon icon="search" />
-            </button>
-          </div>
-        </div>
-        {/* skill List returned from search api */}
-        <div
-          className="div-select-container"
-          style={{ maxHeight: "300px", minHeight: "300px" }}
-        >
-          {props.state.skillOptions && !props.state.loading ? (
-            props.state.skillOptions.map((skillInfo, ndx) => {
-              return (
-                <div
-                  className={
-                    "div-select" +
-                    (props.state.formFields.skillSelect === ndx
-                      ? " selected"
-                      : "")
-                  }
-                  key={ndx}
-                  data-value={ndx}
-                  draggable={true}
-                  onDragStart={event =>
-                    props.handleDragStart(skillInfo, ndx, event)
-                  }
-                  onDragEnd={event => event.stopPropagation()}
-                  onClick={() => props.handleSkillClick(ndx)}
-                  onDoubleClick={props.handleSelect}
-                  title={
-                    `Skill Id: ${skillInfo.id}` +
-                    convertHtmlToText("&#013;&#010;") +
-                    (skillInfo.description
-                      ? skillInfo.description
-                      : "No description available ") +
-                    convertHtmlToText("&#013;&#010;") +
-                    (skillInfo.url ? "URL: " + skillInfo.url : "")
-                  }
-                >
-                  {skillInfo.name}
-                </div>
-              );
-            })
-          ) : (
-            <p>Loading....</p>
-          )}
-        </div>
-        <p>Hover over skill for more details</p>
-        {/* Select and, if prop, Close Buttons */}
-        <div
-          className="search-skills-buttons mt-3"
-          style={{ textAlign: "center" }}
-        >
-          <button
-            type="button"
-            className="btn btn-primary"
-            onClick={props.handleSelect}
-            disabled={
-              !(
-                props.state.skillOptions && props.state.skillOptions.length > 0
-              ) || props.editMode === 1
-            }
-          >
-            {props.searchButton}
-          </button>
-          {props.state.closeBtn && (
-            <button
-              type="button"
-              className="btn btn-warning"
-              onClick={props.state.closeBtn}
-            >
-              Close
-            </button>
-          )}
-        </div>
-      </div>
-    </section>
-  );
+					{/* keyword entry */}
+					<div className="search-field-div">
+						<label htmlFor="keyword">
+							<strong>Search Skills</strong>
+						</label>
+						<div id="skill-search-row" className="row mx-0">
+							<input
+								type="text"
+								className="form-control"
+								name="keyword"
+								id="keyword"
+								value={props.state.formFields.keyword}
+								onChange={props.handleInputChange}
+								onKeyPress={handleKeyPress}
+							/>
+							<button
+								type="button"
+								className="btn btn-dark btn-search mx-1"
+								onClick={props.handleSearch}
+							>
+								<FontAwesomeIcon icon="search" />
+							</button>
+						</div>
+					</div>
+					{/* skill List returned from search api */}
+					<div
+						className="div-select-container"
+						style={{ maxHeight: "300px", minHeight: "300px" }}
+					>
+						{props.state.skillOptions && !props.state.loading ? (
+							props.state.skillOptions.map((skillInfo, ndx) => {
+								return (
+									<div
+										className={
+											"div-select" +
+											(props.state.formFields.skillSelect === ndx
+												? " selected"
+												: "")
+										}
+										key={ndx}
+										data-value={ndx}
+										draggable={true}
+										onDragStart={event =>
+											props.handleDragStart(skillInfo, ndx, event)
+										}
+										onDragEnd={event => event.stopPropagation()}
+										onClick={() => props.handleSkillClick(ndx)}
+										onDoubleClick={props.handleSelect}
+										title={
+											`Skill Id: ${skillInfo.id}` +
+											convertHtmlToText("&#013;&#010;") +
+											(skillInfo.description
+												? skillInfo.description
+												: "No description available ") +
+											convertHtmlToText("&#013;&#010;") +
+											(skillInfo.url ? "URL: " + skillInfo.url : "")
+										}
+									>
+										{skillInfo.name}
+									</div>
+								);
+							})
+						) : (
+							<p>Loading....</p>
+						)}
+					</div>
+					<p>Hover over skill for more details</p>
+					{/* Select and, if prop, Close Buttons */}
+					<div
+						className="search-skills-buttons mt-3"
+						style={{ textAlign: "center" }}
+					>
+						<button
+							type="button"
+							className="btn btn-primary"
+							onClick={props.handleSelect}
+							disabled={
+								!(
+									props.state.skillOptions &&
+									props.state.skillOptions.length > 0
+								) || props.editMode === 1
+							}
+						>
+							{props.searchButton}
+						</button>
+						{props.state.closeBtn && (
+							<button
+								type="button"
+								className="btn btn-warning"
+								onClick={props.state.closeBtn}
+							>
+								Close
+							</button>
+						)}
+					</div>
+				</div>
+			</CardText>
+		</Card>
+	);
 };
 
 export default SkillSearch;
--- src/components/search/SkillSearch/TechtagDropdown.js
+++ src/components/search/SkillSearch/TechtagDropdown.js
@@ -1,71 +1,43 @@
-import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from "react";
+import SelectField from "styledComponents/SelectField";
 
 const TechtagDropdown = ({
-  tagOptions,
-  openTagOptions,
-  tagSelect,
-  handleTagSelect
+	tagOptions = [],
+	openTagOptions,
+	tagSelect,
+	handleTagSelect
 }) => {
-  const displayTechtagSelect = () => {
-    const tagList = tagOptions ? tagOptions : [];
-    const optionHeight = 23;
-    const OptionRows = Math.min(10, tagList.length);
-    let divHeight = openTagOptions ? optionHeight * OptionRows + 6 : 30;
-    return (
-      <div className="div-select-container" style={{ height: divHeight }}>
-        {(tagSelect === -1 || openTagOptions) && (
-          <div
-            className="div-select d-flex justify-content-between tag-option"
-            key={-1}
-            data-value="-1"
-            onClick={() => handleTagSelect(-1)}
-            value="-1"
-          >
-            <div>
-              {tagSelect === -1
-                ? "Select Techtag to Filter Skills"
-                : "Remove Techtag Filter"}
-            </div>
+	const [selectValue, setSelectValue] = useState(tagSelect);
 
-            <div>
-              <FontAwesomeIcon icon="chevron-down" /> &nbsp;
-            </div>
-          </div>
-        )}
-        {tagOptions ? (
-          tagOptions.map((tagInfo, ndx) => {
-            return (
-              (openTagOptions || tagSelect === ndx) && (
-                <div
-                  className={
-                    "div-select d-flex justify-content-between tag-option" +
-                    (tagSelect === ndx ? " selected" : "")
-                  }
-                  key={ndx}
-                  data-value={ndx}
-                  onClick={() => handleTagSelect(ndx)}
-                  title={tagInfo.description}
-                >
-                  <div>{tagInfo.name}</div>
-                  <div>
-                    {tagSelect === ndx && !openTagOptions && (
-                      <FontAwesomeIcon icon="chevron-down" />
-                    )}
-                    &nbsp;
-                  </div>
-                </div>
-              )
-            );
-          })
-        ) : (
-          <p>Loading....</p>
-        )}
-      </div>
-    );
-  };
+	let selectOptions = [{ label: "none", value: -1 }];
 
-  return <div className="form-group">{displayTechtagSelect()}</div>;
+	selectOptions = selectOptions.concat(
+		tagOptions.map((tag, ndx) => {
+			return { label: tag.name, value: ndx };
+		})
+	);
+
+	const handleChange = value => {
+		setSelectValue(value);
+		handleTagSelect(value);
+	};
+
+	const displayTechtagSelect = () => {
+		return (
+			<SelectField
+				id="select-techtag"
+				label="Filter By Techtag"
+				placeholder="Techtag"
+				menuItems={selectOptions}
+				simplifiedMenu={true}
+				value={selectValue}
+				onChange={handleChange}
+				style={{ width: "100%" }}
+			/>
+		);
+	};
+
+	return displayTechtagSelect();
 };
 
 export default TechtagDropdown;
