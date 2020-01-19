b12d5f34ea8cc9b8aaea754d6da2432ab719ea07
Ron Boutilier <ronbout@yahoo.com>
2020-01-19 13:09:49 -0600

    Converted all inpUrl entries to string before running regex test.  Numeric data was triggering an error because it has no .match() function

--- src/App.js
+++ src/App.js
@@ -54,8 +54,8 @@ library.add(
 // setup global api url
 // if not on my dev, use remote api
 // const API_LOC = "local";
+const API_LOC = "azure";
 // const API_LOC = "local";
-const API_LOC = "local";
 
 if (API_LOC === "local") {
 	window.imgLoc = "http://localhost/3sixd/imgs/";
